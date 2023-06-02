23e1d22079732e035ff4ee696236937b189b8999
boga0107 <gabriel.boehm11@gmail.com>
2023-06-02 15:42:56 +0200

    added feature - blinking led

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,15 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  pinMode(18, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(18, HIGH);
+  delay(1000);
+  digitalWrite(18, LOW);
+  delay(1000);
 }
 
 // put function definitions here:
