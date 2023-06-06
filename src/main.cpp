705e517c9cdaa57251327dd14acb96f7211c337a
Dennis <scde1044@h-ka.de>
2023-06-06 23:36:27 +0200

    InitialCommit

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,20 @@
 #include <Arduino.h>
-#include <AccelStepper.h>              
+hw_timer_t* timer = NULL;
 
-AccelStepper stepper(AccelStepper::FULL4WIRE,33,32,25,14,true);  // 4 wire full stepper
-long steering_val=0;
-
-void setup() {
-  /*
-  Hier evtl. IN- bzw. OUTPUTS deklarieren, sollte jedoch in der AccelStepper Library automatisch passieren
-  */
-  stepper.setAcceleration(5000);        // Acceleration in steps/s^2
-  stepper.setMaxSpeed(5000);            // Speed in steps/s
-  stepper.setSpeed(2000);
-  stepper.enableOutputs();
-  //stepper.setOutputPins(AccelStepper::FULL4WIRE);
-  stepper.setMinPulseWidth(20);         // 20 microseconds
+void IRAM_ATTR onTimer()
+{
+  Serial.println("Milliseconds: "+ String(millis()));
 }
 
-void loop() 
-{
-  //if(uart.any())
-  //{ 
-      stepper.moveTo(steering_val*(-10)); // negative anticlockwise
-      while(stepper.distanceToGo()!=0)
-      {
-        stepper.run();
-      }
-  //}
-} 
+void setup() {
+  timer = timerBegin(0,80,true); //Timer_0, prescaling: 80MHz / 80 = 1MHz -> T=1 us
+  timerAttachInterrupt(timer, onTimer, true); //Interrupt Funktion onTimer()
+  timerAlarmWrite(timer, 1000000, true); //Faktor für Timer z.B. 1000000*1us = 1s
+  // Faktor muss evtl halbiert werden. Aktueller Timer zählt 2 Sekunden lang 
+  timerAlarmEnable(timer);
+  Serial.begin(115200);
+}
+
+void loop() {
+  delay(10); // this speeds up the simulation
+}
