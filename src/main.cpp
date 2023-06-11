3642674f823ac68edde1f8ca4248a9b18b30502a
Isabel Kraus <kris1011@h-ka.de>
2023-06-12 00:21:02 +0200

    sensors: initial commit; sensor basic functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-
+#include <Ultrasonic.h>
+#include <sensors.cpp>
 
 // Pin Belegung
 #define out_driveDirection  GPIO_NUM_5
@@ -13,12 +14,32 @@
 #define sensor3_echo        GPIO_NUM_13
 
 
+Ultrasonic sensor1(sensor1_trigger, sensor1_echo);
+Ultrasonic sensor2(sensor2_trigger, sensor2_echo);
+Ultrasonic sensor3(sensor3_trigger, sensor3_echo);
+
 
 void setup() {
   // put your setup code here, to run once:
+
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  sensors[0] = sensor1.read(); //Liest die Distanz in cm
+  sensors[1] = sensor2.read();
+  sensors[2] = sensor3.read();
+
+  for(int i = 0; i<3; i++){
+    if(distanceCheck(emergencyBreakValue, i) == false){
+      //before interrupting check value again to avoid ghosts
+      //interrupt, emergency break!
+      printf("EMERGENCY BREAK");
+    } //else continue;
+  }
+
+  delay(1000); //pause für 1 s, eventuell nicht nötig. (Test)
 }
+
 
---
+++ src/sensors.cpp
@@ -0,0 +1,43 @@
+#include <Ultrasonic.h>
+
+//class to define the HC-SR04 Sensors
+//contains:
+// read distance
+// ghost avoidance --- to implement
+// security definitions of emergency break
+
+// int safetyCheck = 0; //Check which sensor got a signal
+int emergencyBreakValue = 70; //Distance to trigger emergency break. Now set to default = 70 cm. 
+int sensors[3];
+
+// check if distance is below emergency break distance. 
+// emergency distance is kept as parameter to be more accessible during programming than a fixed value
+bool distanceCheck(int emergencyBreak, int sensorNumber)
+{
+  switch(sensorNumber)
+  {
+    case 0:
+      if (sensors[0] > emergencyBreak){
+        //safetyCheck++;
+        return true;
+      }
+    case 1:
+      if (sensors[1] > emergencyBreak){
+        //safetyCheck++;
+        return true;
+      }
+    case 2:
+      if (sensors[2] > emergencyBreak){
+        //safetyCheck++;
+        return true;
+      }
+    default:
+      return false;
+  }
+}
+
+// to check if sensors are correct (triplet-system based)
+// to be implemented in further steps
+bool securityCheck(int sensorNumber){
+
+}
