48ef1f615819c277db40b9c7abb18c094580c004
boga0107 <gabriel.boehm11@gmail.com>
2023-06-13 19:22:13 +0200

    Antriebsmotor Klasse komplett übersetzt

--- include/antriebsmotor.cpp
+++ include/antriebsmotor.cpp
@@ -1,18 +1,38 @@
 #include "antriebsmotor.h"
 
 antrieb::antrieb(uint8_t &pThrottlePin, uint8_t &pDirectionPin):
-    mThrottlePin(pThrottlePin), mDirectionPin(pDirectionPin){
-    
+    mThrottlePin(pThrottlePin), mDirectionPin(pDirectionPin), mSpeed(0), mDirection(0){
+    // PWM setup
+    ledcSetup(PWM_CHANNEL, PWM_FREQUENCY, PWM_RESOLUTION);
+    // attach pwm channel to GPIO pin
+    ledcAttachPin(mThrottlePin, PWM_CHANNEL);
+
+    // Direction Pin setup
+    pinMode(mDirection, OUTPUT);
+
+    // set motor to speed 0
+    setSpeed(mSpeed);
+    // set direction to forward
+    setDirection(mDirection);    
 }
 
 boolean antrieb::setDirection(boolean &pDirection){
-
+    mDirection = pDirection;
+    digitalWrite(mDirectionPin, mDirection);
+    return true;
 }
 
 boolean antrieb::setSpeed(uint16_t &pSpeed){
-
+    mSpeed = pSpeed;
+    ledcWrite(PWM_CHANNEL, mSpeed);
+    return true;
 }
 
 boolean antrieb::setSaveState(){
-    
+    mDirection= 0;
+    setDirection(mDirection);
+
+    mSpeed = 270;
+    setSpeed(mSpeed);
+    return true;
 }
--- include/antriebsmotor.h
+++ include/antriebsmotor.h
@@ -3,13 +3,19 @@
 
 #include "pins.h"
 
+#define PWM_FREQUENCY 5000
+#define PWM_CHANNEL 0
+#define PWM_RESOLUTION 10
+const uint16_t MAX_DUTY_CYCLE = (int)(pow(2, PWM_RESOLUTION)-1);
+
 
 class antrieb {
 private:
     const uint8_t mThrottlePin;
     const uint8_t mDirectionPin;
+
     uint16_t mSpeed;
-    boolean mDirection; 
+    boolean mDirection;
         // TRUE = backwards
         // FALSE = forward
     
