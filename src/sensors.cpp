3451959536cd494da69b8c6b91fd00968d5f8098
Isabel Kraus <kris1011@h-ka.de>
2023-06-20 19:23:49 +0200

    comment update

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -7,9 +7,10 @@
 // security definitions of emergency break
 //check if distance is either below emergency break distance or above maximal distance measued.
 
-int emergencyDistanceValue = 70;
-int maximalDistanceValue = 350;
+int emergencyDistanceValue = 70; //fixed value
+int maximalDistanceValue = 350; //fixed value
 
+// check if distance is below emergency break distance. 
 void distanceCheck(int sensorValue)
 {
   if(sensorValue < emergencyDistanceValue){
@@ -19,9 +20,6 @@ void distanceCheck(int sensorValue)
     Serial.println("too far away ");
   }
 }
-
-// check if distance is below emergency break distance. 
-// emergency distance is kept as parameter to be more accessible during programming than a fixed value
 
 // to check if sensors are correct (triplet-system based)
 // to be implemented in further steps
