a0c71a1aa1a980bb302da76fdb9d36d54766a052
boga0107 <gabriel.boehm11@gmail.com>
2023-08-16 18:20:47 +0200

    ultrasonic object init moved to constructor

--- include/sensors.cpp
+++ include/sensors.cpp
@@ -1,25 +1,28 @@
 #include "sensors.h"
 
-sensor::sensor(uint8_t TriggerPin1, uint8_t TriggerPin2, uint8_t TriggerPin3,
-               uint8_t EchoPin1, uint8_t EchoPin2, uint8_t EchoPin3, Break &pBreak) :
-
-                                                                                      sensorTriggerPin1(TriggerPin1), sensorTriggerPin2(TriggerPin2), sensorTriggerPin3(TriggerPin3),
-                                                                                      sensorEchoPin1(EchoPin1), sensorEchoPin2(EchoPin2), sensorEchoPin3(EchoPin3), myBreak(pBreak)
+sensor::sensor(uint8_t TriggerPin1, uint8_t TriggerPin2,
+               uint8_t TriggerPin3, uint8_t EchoPin1,
+               uint8_t EchoPin2, uint8_t EchoPin3, Break &pBreak) : sensorTriggerPin1(TriggerPin1), sensorTriggerPin2(TriggerPin2),
+                                                                    sensorTriggerPin3(TriggerPin3), sensorEchoPin1(EchoPin1),
+                                                                    sensorEchoPin2(EchoPin2), sensorEchoPin3(EchoPin3), myBreak(pBreak),
+                                                                    sensor1(sensorTriggerPin1, sensorEchoPin1),
+                                                                    sensor2(sensorTriggerPin2, sensorEchoPin2),
+                                                                    sensor3(sensorTriggerPin3, sensorEchoPin3)
 {
 }
 
-void sensor::setDistance(uint16_t value, uint8_t number)
+void sensor::setDistance(uint16_t pDistance, uint8_t sensorIndex)
 {
-  switch (number)
+  switch (sensorIndex)
   {
   case 1:
-    distance1 = value;
+    distance1 = pDistance;
     break;
   case 2:
-    distance2 = value;
+    distance2 = pDistance;
     break;
   case 3:
-    distance3 = value;
+    distance3 = pDistance;
     break;
   default:
     // do nothing for now
@@ -30,9 +33,6 @@ void sensor::setDistance(uint16_t value, uint8_t number)
 /* read the distance and break if necessary*/
 void sensor::readDistance()
 {
-  Ultrasonic sensor1(sensorTriggerPin1, sensorEchoPin1);
-  Ultrasonic sensor2(sensorTriggerPin2, sensorEchoPin2);
-  Ultrasonic sensor3(sensorTriggerPin3, sensorEchoPin3);
   setDistance(sensor1.read(), 1);
   setDistance(sensor2.read(), 2);
   setDistance(sensor3.read(), 3);
@@ -41,9 +41,9 @@ void sensor::readDistance()
     myBreak.Activate_EmergencyBreak();
 }
 
-uint16_t sensor::getDistance(uint8_t number)
+uint16_t sensor::getDistance(uint8_t sensorIndex)
 {
-  switch (number)
+  switch (sensorIndex)
   {
   case 1:
     return distance1;
@@ -52,7 +52,7 @@ uint16_t sensor::getDistance(uint8_t number)
   case 3:
     return distance3;
   default:
-    return 0;
+    return 0;  /* might cause an emergency break */
   }
 }
 
@@ -62,7 +62,7 @@ bool sensor::distanceOK()
   bool distanceState = true;
   for (int i = 1; i < 4; i++)
   {
-    if (getDistance(i) < emergencyDistanceValue)
+    if (getDistance(i) < EMERGENCY_DISTANCE_VALUE)
     {
       distanceState &= false;
     }
@@ -72,7 +72,7 @@ bool sensor::distanceOK()
 
 // to check if sensors are correct (triplet-system based)
 // to be implemented in further steps
-bool sensor::securityCheck(uint8_t sensorNumber)
+bool sensor::securityCheck(uint8_t sensorIndex)
 {
   return true;
 }
--- include/sensors.h
+++ include/sensors.h
@@ -1,21 +1,24 @@
-#ifndef sensors 
-#define sensors
+#ifndef SENSORS_H
+#define SENSORS_H
 #include <Arduino.h>
 #include <Ultrasonic.h>
 #include "bremse.h"
 
-//class to define the HC-SR04 Sensors
-//contains:
-// read distance
-// ghost avoidance --- to implement
-// security definitions of emergency break
-//check if distance is either below emergency break distance or above maximal distance measued.
+/* class to define the HC-SR04 Sensors
+ * contains:
+ * read distance
+ * ghost avoidance --- to implement
+ * security definitions of emergency break
+ * check if distance is either below emergency break distance or above maximal distance measued.
+ * */
 
-class sensor {
+#define EMERGENCY_DISTANCE_VALUE 70
+#define MAXIMAL_DISTANCE_VALUE 350
+
+class sensor
+{
 
 private:
-    const uint16_t emergencyDistanceValue = 70;
-    const uint16_t maximalDistanceValue = 350;
     const uint8_t sensorTriggerPin1;
     const uint8_t sensorTriggerPin2;
     const uint8_t sensorTriggerPin3;
@@ -26,17 +29,20 @@ private:
     uint16_t distance2;
     uint16_t distance3;
 
+    Ultrasonic sensor1;
+    Ultrasonic sensor2;
+    Ultrasonic sensor3;
+
     Break &myBreak;
 
 public:
-    sensor(uint8_t TriggerPin1, uint8_t TriggerPin2, uint8_t TriggerPin3, uint8_t EchoPin1
-    , uint8_t EchoPin2, uint8_t EchoPin3, Break &pBreak);
+    sensor(uint8_t TriggerPin1, uint8_t TriggerPin2, uint8_t TriggerPin3,
+           uint8_t EchoPin1, uint8_t EchoPin2, uint8_t EchoPin3, Break &pBreak);
 
     void readDistance();
-    void setDistance(uint16_t value, uint8_t number);
-    uint16_t getDistance(uint8_t number);
-    void distanceCheck();
+    void setDistance(uint16_t pDistance, uint8_t sensorIndex);
+    uint16_t getDistance(uint8_t sensorIndex);
     bool distanceOK();
-    bool securityCheck(uint8_t sensorNumber);
+    bool securityCheck(uint8_t sensorIndex);
 };
 #endif
